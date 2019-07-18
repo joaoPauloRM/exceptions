a8279a3473299d53a124dcb943c2096ddcdfe4ff
BeniGod <jpmrs96@gmail.com>
2019-07-18 09:19:53 -0300

    With exceptions

--- src/com/model/entities/Reservation.java
+++ src/com/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import com.model.exceptions.DomainException;
+
 public class Reservation {
 
 	private Integer roomNumber;
@@ -11,10 +13,11 @@ public class Reservation {
 	private Date checkOut;
 
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
-	
 
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		if (checkIn.after(checkOut)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -42,6 +45,13 @@ public class Reservation {
 	}
 
 	public void updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			throw new DomainException("Reservation dates for update must be future dates");
+		}
+		if (checkIn.after(checkOut)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 	}
